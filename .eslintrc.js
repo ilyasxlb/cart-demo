585284b8b4e74576f7e153d0dee8669c73711533
Ilyas Latypov <ilyasxlb@gmail.com>
2025-04-19 13:00:24 +0400

    feat: add ESLint, prettier and import aliases config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,74 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['import', 'prettier'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './',
+      },
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        alias: {
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@services': './src/services',
+          '@navigation': './src/navigation',
+          '@assets': './assets',
+        },
+      },
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'import/no-unresolved': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@components/**',
+            group: 'internal',
+          },
+          {
+            pattern: '@screens/**',
+            group: 'internal',
+          },
+          {
+            pattern: '@services/**',
+            group: 'internal',
+          },
+          {
+            pattern: '@navigation/**',
+            group: 'internal',
+          },
+          {
+            pattern: '@assets/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@navigation': './src/navigation',
+          '@assets': './assets',
+        },
+      },
+    ],
+  ],
 };
